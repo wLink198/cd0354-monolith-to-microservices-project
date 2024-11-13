c6563136e68ea84c12f2ccfbd42436770cc50f17
wlink198 <linhwhite98@gmail.com>
2024-11-13 13:58:33 +0700

    update frontend config for api host

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://ab33b77a44522413d93673c9085cfceb-643323764.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
